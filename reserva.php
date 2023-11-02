769026dc2cdebb5d73eaa5d5533e81090ac71908
Haniel Carvalho <78237588+HanielCS@users.noreply.github.com>
2023-11-02 16:55:35 -0300

    Update reserva.php

--- reserva.php
+++ reserva.php
@@ -1,33 +1,41 @@
-<? 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica se os campos do formulário estão definidos
+    if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['data']) && isset($_POST['hora']) && isset($_POST['num_pessoas']) && isset($_POST['mensagem'])) {
+        $destinatario = "carvalho.hanielx@gmail.com.br";
 
-$destinatario = "carvalho.hanielx@gmail.com.br";
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $data = $_POST['data'];
+        $hora = $_POST['hora'];
+        $num_pessoas = $_POST['num_pessoas'];
+        $mensagem = $_POST['mensagem'];
+        $assunto = "Assunto da Reserva";
 
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING); // filter_input filtra os dados
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL); // filter_validate_email valida o email
-$telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING); // filter_sanitize_string limpa os caracteres especiais
-$data = filter_input(INPUT_POST, 'data', FILTER_SANITIZE_STRING); // input_post recebe os dados do formulario
-$hora = filter_input(INPUT_POST, 'hora', FILTER_SANITIZE_STRING);
-$num_pessoas = filter_input(INPUT_POST, 'num_pessoas', FILTER_SANITIZE_STRING);
-$mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
-$assunto = filter_input(INPUT_POST, 'assunto', FILTER_SANITIZE_STRING);
+        // Corpo do e-mail
+        $body = "===============================\n";
+        $body .= "RESERVA\n";
+        $body .= "===============================\n";
+        $body .= "Nome: " . $nome . "\n";
+        $body .= "Email: " . $email . "\n";
+        $body .= "Telefone: " . $telefone . "\n";
+        $body .= "Data: " . $data . "\n";
+        $body .= "Hora: " . $hora . "\n";
+        $body .= "Número de pessoas: " . $num_pessoas . "\n";
+        $body .= "Mensagem: " . $mensagem . "\n";
 
-$body = "===============================" . "\n");
-$body .= "RESERVA" . "\n";
-$body .= "===============================" . "\n";
-$body .= "Nome: " . $nome . "\n";
-$body .= "Email: " . $email . "\n";
-$body .= "Telefone: " . $telefone . "\n";
-$body .= "Data: " . $data . "\n";
-$body .= "Hora: " . $hora . "\n";
-$body .= "Número de pessoas: " . $num_pessoas . "\n";
-$body .= "Mensagem: " . $mensagem . "\n";
+        $headers = "From: " . $email . "\r\n"; // Endereço de e-mail do remetente
 
-$headers = "From: " . $email . "\r\n";
-
-if(mail($destinatario, $assunto, $body, $headers)) {
-  echo "E-mail enviado com sucesso!";
+        if(mail($destinatario, $assunto, $body, $headers)) {
+            echo "E-mail enviado com sucesso!";
+        } else {
+            echo "Erro ao enviar o e-mail.";
+        }
+    } else {
+        echo "Todos os campos do formulário devem estar preenchidos.";
+    }
 } else {
-  echo "E-mail não enviado!";
+    echo "Método de requisição inválido.";
 }
-
 ?>
