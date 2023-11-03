c99b6712593a714d03f8e9864faa08ca7545912e
Haniel Carvalho <78237588+HanielCS@users.noreply.github.com>
2023-11-03 13:25:53 -0300

    Apenas slide
    Realocação de codigo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var myIndex = 0; // Variável para controlar o slide
   slide(); // Chamada de função para iniciar assim que a página for carregada
 
@@ -9,28 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
       x[i].style.display = "none"; // Oculta todas as imagens do slide
     }
     myIndex++; // Incremento de valor para passar imagem
-    if (myIndex > x.length) { 
-      myIndex = 1; // Define myIndex como 1 se todas as imagens já tiverem passado
-    }
+    if (myIndex > x.length) { myIndex = 1 } // Define myIndex como 1 se todas as imagens ja tiverem passado
     x[myIndex - 1].style.display = "block"; // Exibe a imagem atual
-    setTimeout(slide, 5000); // 5 segundos
+    setTimeout(slide, 5000); // 1000 = 1 segundo
   }
 
-  const links = document.querySelectorAll('a[href^="#"]'); // Captura os links do menu
-
-  links.forEach(link => { // Adicione um parâmetro (link) para representar o elemento do array
-    link.addEventListener('click', function(e) {
-      e.preventDefault(); // Previne o comportamento padrão do link
-
-      const targetID = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetID);
-
-      const offsetTop = targetElement.offsetTop;
-
-      window.scrollTo({
-        top: offsetTop,
-        behavior: 'smooth'
-      });
-    });
-  });
 });
