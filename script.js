4b60d9b12a44da334f7ff72af482b988fe5a1f36
Haniel Carvalho <78237588+HanielCS@users.noreply.github.com>
2023-11-03 13:18:49 -0300

    Identação e Comentarios
    Correção na identação e escrita dos comentários mais clara

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var myIndex = 0; // Variável para controlar o slide
+  slide(); // Chamada de função para iniciar assim que a página for carregada
 
-var myIndex = 0; // Variável para controlar o slide
-slide (); // Chamada de função para iniciar assim que a página for carregada
-
-function slide() {
-  var i;
-  var x = document.getElementsByClassName("mySlides"); // Seleciona todos os elementos com a classe mySlides
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; // Oculta todas as imagens do slide
+  function slide() {
+    var i;
+    var x = document.getElementsByClassName("mySlides"); // Seleciona todos os elementos com a classe mySlides
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; // Oculta todas as imagens do slide
+    }
+    myIndex++; // Incremento de valor para passar imagem
+    if (myIndex > x.length) { 
+      myIndex = 1; // Define myIndex como 1 se todas as imagens já tiverem passado
+    }
+    x[myIndex - 1].style.display = "block"; // Exibe a imagem atual
+    setTimeout(slide, 5000); // 5 segundos
   }
-  myIndex++; // Incremento de valor para passar imagem
-  if (myIndex > x.length) { myIndex = 1 } // Define myIndex como 1 se todas as imagens ja tiverem passado
-  x[myIndex - 1].style.display = "block"; // Exibe a imagem atual
-  setTimeout(slide, 5000); // 1000 = 1 segundo
-}
 
   const links = document.querySelectorAll('a[href^="#"]'); // Captura os links do menu
 
