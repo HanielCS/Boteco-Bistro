7c0c1e9afb7875e6db80004b71213de0b79134aa
Haniel Carvalho <carvalho.hanielx@gmail.com>
2023-11-04 11:02:33 -0300

    Addslahses e nomes
    Adição da função addslashes para captar melhor as strings e mudança no nome de algumas variaveis

--- reserva.php
+++ reserva.php
@@ -2,16 +2,17 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Verifica se os campos do formulário estão definidos
     if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['data']) && isset($_POST['hora']) && isset($_POST['num_pessoas']) && isset($_POST['mensagem'])) {
-        $destinatario = "carvalho.hanielx@gmail.com.br";
 
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $telefone = $_POST['telefone'];
-        $data = $_POST['data'];
-        $hora = $_POST['hora'];
-        $num_pessoas = $_POST['num_pessoas'];
-        $mensagem = $_POST['mensagem'];
-        $assunto = "Assunto da Reserva";
+        $nome = addslashes($_POST['nome']);
+        $email = addslashes($_POST['email']);
+        $telefone = addslashes$_POST(['telefone']);
+        $data = addslashes($_POST['data']);
+        $hora = addslashes($_POST['hora']);
+        $num_pessoas = addslashes($_POST['num_pessoas']);
+        $mensagem = addslashes($_POST['mensagem']);
+
+        $to = "carvalho.hanielx@gmail.com.br"; // E-mail que vai receber o form
+        $subject = "Reserva de Serviço"; // Assunto do E-mail
 
         // Corpo do e-mail
         $body = "===============================\n";
@@ -25,9 +26,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $body .= "Número de pessoas: " . $num_pessoas . "\n";
         $body .= "Mensagem: " . $mensagem . "\n";
 
-        $headers = "From: " . $email . "\r\n"; // Endereço de e-mail do remetente
+        $header = "From:contato.haniel.c@gmail.com"."\r\n".
+                  "Reply-To".$email."\r\n".
+                  "X=Mailer:PHP/".phpversion();
 
-        if(mail($destinatario, $assunto, $body, $headers)) {
+        if(mail($to, $subject, $body, $header)) {
             echo "E-mail enviado com sucesso!";
         } else {
             echo "Erro ao enviar o e-mail.";
