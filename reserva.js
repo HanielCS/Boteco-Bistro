e9888b10bb518c70cd0557be21757b8ab368061f
HanielCS <carvalho.hanielx@gmail.com>
2023-11-05 16:25:02 -0300

    Melhoria na Reserva

--- reserva.js
+++ reserva.js
@@ -8,17 +8,28 @@ function abrirWhatsapp() {
   var msg = document.getElementById("msg").value;
 
   var num_telefone = "+5575991232281";
-  
+
   var url = "https://wa.me/" + num_telefone + "?text=" +
-  "*Formulário de Reserva*" +
-  "%0a" +
-  "*Nome:* " + nome + "%0a" +
-  "*Email:* " + email + "%0a" +
-  "*Telefone:* " + telefone + "%0a" +
-  "*Data:* " + data + "%0a" +
-  "*Hora:* " + hora + "%0a" +
-  "*Número de pessoas:* " + num_pessoas + "%0a" +
-  "*Mensagem:* " + msg;
-  
+    "*Formulário de Reserva*" +
+    "%0a" +
+    "*Nome:* " + nome + "%0a" +
+    "*Email:* " + email + "%0a" +
+    "*Telefone:* " + telefone + "%0a" +
+    "*Data:* " + data + "%0a" +
+    "*Hora:* " + hora + "%0a" +
+    "*Número de pessoas:* " + num_pessoas + "%0a" +
+    "*Mensagem:* " + msg;
+
   window.open(url, '_blank').focus();
 }
+
+document.getElementById("enviarEmail").addEventListener("click", function(event) {
+  var form = document.getElementById("formReserva");
+  event.preventDefault();
+  form.submit();
+});
+
+document.getElementById("enviarWhatsapp").addEventListener("click", function(event) {
+  event.preventDefault();
+  abrirWhatsapp();
+});
